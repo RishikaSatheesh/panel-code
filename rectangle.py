cbba87472512307b8216ca90a9da50e4759b71ad
RishikaSatheesh <57759832+RishikaSatheesh@users.noreply.github.com>
2025-02-05 16:11:27 +0530

    Create rectangle.py
    Contains functions that return panel components to be displayed in the application.

---
+++ rectangle.py
@@ -0,0 +1,105 @@
+import param
+import panel as pn
+import pandas as pd
+from common import Common
+import logging 
+from datetime import datetime
+log_dir = '/localpart0/aop-shared/WAVES_PDEV/logs/spt/retention'
+log = '{0}/error_poc_{1}.log'.format(
+                            log_dir,
+                            datetime.now().strftime(str('%Y_%m'))
+                        )
+username = pn.state.session_args.get(
+                                    'username', [b"username"]
+                                )[0].decode().upper()
+logging.basicConfig(
+                filename=log,
+                filemode='a',
+                format=f'%(asctime)s - %(levelname)s - {username} - %(message)s',
+                level=logging.INFO,
+                force=True
+            )
+
+class Button(param.Parameterized):
+    # tab specific submit button
+    submit_button = param.Action(lambda x: x.param.trigger('submit_button'), label='Submit')
+
+class rectangle(param.Parameterized):
+    length1 = param.Number(default=1, step=1, bounds=(1, 100))
+    width1 = param.Number(default=1, step=1, bounds=(1, 100))
+    length2 = param.Number(default=1, step=1, bounds=(1, 100))
+    width2 = param.Number(default=1, step=1, bounds=(1, 100))
+    button = Button()
+    common = param.ClassSelector(class_=Common, default=Common())
+    df = pd.DataFrame()
+
+    def get_data(self):
+        lengths = []
+        widths = []
+        perimeters = []
+        areas = []
+
+        for i in range(1, self.common.number_of_shapes + 1):
+            lengths.append(getattr(self, f'length{i}'))
+            widths.append(getattr(self, f'width{i}'))
+            perimeters.append(2 * (lengths[i - 1] + widths[i - 1]))
+            areas.append(lengths[i - 1] * widths[i - 1])
+
+        self.df = pd.DataFrame({
+            'Length': lengths,
+            'Width': widths,
+            'Perimeter': perimeters,
+            'Area': areas
+        })
+
+    @param.depends('common.select_button')
+    def get_first_dashboard(self):
+        dimensions = pn.GridBox(ncols=self.common.number_of_shapes)
+        for i in range(self.common.number_of_shapes):
+            dimensions.append(
+                pn.WidgetBox(
+                    pn.Param(
+                        self,
+                        parameters=[f'length{i + 1}'],
+                        widgets={
+                            f'length{i + 1}': {
+                                'widget_type': pn.widgets.IntInput,
+                                'name': 'Length:',
+                                'width': 115
+                            }
+                        },
+                        show_name=False,
+                        default_layout=pn.Column
+                    ),
+                    pn.Param(
+                        self,
+                        parameters=[f'width{i + 1}'],
+                        widgets={
+                            f'width{i + 1}': {
+                                'widget_type': pn.widgets.FloatInput,
+                                'name': 'Width:',
+                                'width': 115
+                            }
+                        },
+                        show_name=False,
+                        default_layout=pn.Column
+                    ),
+                    margin=(4, 2, 4, 2)
+                )
+            )
+
+        return dimensions
+
+    def get_submit_button(self):
+        return pn.Param(
+            self.button,
+            parameters=['submit_button'],
+            widgets={'submit_button': {'button_type': 'primary'}},
+            show_name=False
+        )
+
+    @pn.depends('common.select_button', 'button.submit_button')
+    def get_second_dashboard(self):
+        self.get_data()
+        logging.info("Done")
+        return pn.Card(pn.widgets.Tabulator(self.df))
